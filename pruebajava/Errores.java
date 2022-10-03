3a2c5284268ebe98bfe7eae79a4f5ec83613d0d1
Perillos <valero.ce@gmail.com>
2022-10-03 13:21:01 +0200

    practica1

---
+++ pruebajava/Errores.java
@@ -0,0 +1,6 @@
+public class Errores {
+    public static void main(String[] args) {
+        System.out.print ("Pepito Perez) // Error de cerrar comillas y el ; final
+    }
+
+}
---
+++ pruebajava/Program.java
@@ -0,0 +1,6 @@
+class Programa{
+    public static void main(String[] args){
+        System.out.print ("Hola programadores, soy " + args[0]);
+//args[0] permitirá insertar un argumento
+    }
+}
